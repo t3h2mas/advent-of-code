df83f6c2318a6194b3d542a1e2f941bcef3431ac
Thomas Noe <2mas@hey.com>
2021-12-01 14:34:52 -0700

    Fix newline split for AdventIO.as_lines

--- 2021/ruby/day-one/lib/advent_io.rb
+++ 2021/ruby/day-one/lib/advent_io.rb
@@ -1,5 +1,5 @@
 module AdventIO
   def self.as_lines(file)
-    file.read.split('\n').filter { |line| line.length > 0 }
+    file.read.split("\n").filter { |line| line.length > 0 }
   end
 end
--- 2021/ruby/day-one/spec/advent_io_spec.rb
+++ 2021/ruby/day-one/spec/advent_io_spec.rb
@@ -2,7 +2,7 @@ require 'advent_io'
 
 RSpec.describe 'AdventIO' do
   describe 'as_lines' do
-    let(:file) { StringIO.new('\n123\n321\n\n\n') }
+    let(:file) { StringIO.new("\n123\n321\n\n\n") }
 
     it 'reads lines while handling whitespace' do
       expect(AdventIO.as_lines(file)).to eq(['123', '321'])
