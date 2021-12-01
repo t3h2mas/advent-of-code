e6a60ced150c53ce2c340462bab4aacc8961e545
Thomas Noe <2mas@hey.com>
2021-12-01 13:50:51 -0700

    close expect

--- 2021/ruby/day-one/spec/advent_io_spec.rb
+++ 2021/ruby/day-one/spec/advent_io_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'AdventIO' do
     let(:file) { StringIO.new('\n123\n321\n\n\n') }
 
     it 'reads lines while handling whitespace' do
-      expect(AdventIO.as_lines(file).to eq(['123', '321'])
+      expect(AdventIO.as_lines(file)).to eq(['123', '321'])
     end
   end
 end
